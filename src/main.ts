44932f3b53d0a68365c3d91d12091446c34d75c6
Osmin Ariel López Claros <31580071+Ariel2294@users.noreply.github.com>
2022-09-01 00:06:33 -0600

    feat: added logger config (#3)
    Co-authored-by: Osmin Lopez <arielopez229422@hotmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
 import { AppModule } from './app.module';
+import { loggerOptions } from './utils/logger';
 BigInt.prototype['toJSON'] = function () {
   return this.toString();
 };
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger(loggerOptions),
+  });
 
   const configService = app.get(ConfigService);
 
@@ -15,7 +19,7 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(port, () => {
-    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
 }
 bootstrap();

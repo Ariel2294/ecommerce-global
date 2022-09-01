81aa79dbebfa8be5aef38b853b42080feab75416
Osmin Ariel López Claros <31580071+Ariel2294@users.noreply.github.com>
2022-09-01 09:26:54 -0600

    feat: added config mongodb connection (#4)
    Co-authored-by: Osmin Lopez <arielopez229422@hotmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EcommerceGlobalConfig } from './config/ecommerce-global.config';
 import { EcommerceGlobalModule } from './config/ecommerce-global.module';
 import { EcommerceGlobalServiceValidateConfig } from './config/ecommerce-global.validate.config';
 
 @Module({
   imports: [
     EcommerceGlobalModule,
+    MongooseModule.forRootAsync({
+      imports: [EcommerceGlobalModule],
+      useFactory: async (configService: EcommerceGlobalConfig) => ({
+        uri: configService.databaseURL,
+      }),
+      inject: [EcommerceGlobalConfig],
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       validate: EcommerceGlobalServiceValidateConfig,

2c296895c79e4a4668cfeb63a0e35e3d50adaff8
Osmin Ariel López Claros <31580071+Ariel2294@users.noreply.github.com>
2022-09-01 10:28:44 -0600

    feat: added swagger documentation api config (#5)
    Co-authored-by: Osmin Lopez <arielopez229422@hotmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { WinstonModule } from 'nest-winston';
 import { AppModule } from './app.module';
 import { loggerOptions } from './utils/logger';
@@ -15,8 +16,40 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>('PORT') || 3000;
-
   const globalPrefix = 'api';
+
+  const environment =
+    configService.get<string>('NODE_ENV') || process.env.NODE_ENV;
+
+  const config = new DocumentBuilder()
+    .addApiKey(
+      { type: 'apiKey', name: 'Authorization', in: 'header' },
+      'StaticToken',
+    )
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+    )
+    .setTitle('Ecommerce global api demo')
+    .setDescription(
+      'This is the api documentation for ecommerce global service',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  if (environment === 'develop') {
+    SwaggerModule.setup('docs', app, document, {
+      swaggerOptions: { defaultModelsExpandDepth: -1 },
+    });
+  }
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
